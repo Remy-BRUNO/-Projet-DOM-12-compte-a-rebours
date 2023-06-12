0cca0e2426ca13fe75b0769c4b6f24738a54898b
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-06-12 13:44:04 +0200

    correction commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const day = document.querySelector(".day")
 const hours = document.querySelector(".hours")
 const minute = document.querySelector(".min")
 const seconde = document.querySelector(".sec")
+const counters = document.querySelector(".product-counter")
+const countersEnd = document.querySelector(".counter-end")
 
 // date d'achat limite
 const dateLimit = new Date(2023, 5, 19, 9, 30, 0)
@@ -29,16 +31,32 @@ function cooldown() {
   const limitInMs = dateLimit.getTime()
 
   const timer = (limitInMs - presentInMs) / 1000
+  if (limitInMs <= presentInMs) {
+    counters.style.display = "none"
+    countersEnd.style.display = "block"
+  } else {
+    const timerSec = Math.floor(timer % 60)
+    const timerMin = Math.floor((timer / 60) % 60)
+    const timerHrs = Math.floor((timer / 60 / 60) % 24)
+    const timerDays = Math.floor(timer / 60 / 60 / 24)
+    if (timerSec < 10) {
+      seconde.textContent = `0${timerSec}`
+    } else {
+      seconde.textContent = timerSec
+    }
+    if (timerMin < 10) {
+      minute.textContent = `0${timerMin}`
+    } else {
+      minute.textContent = timerMin
+    }
+    if (timerHrs < 10) {
+      hours.textContent = `0${timerHrs}`
+    } else {
+      hours.textContent = timerHrs
+    }
 
-  const timerSec = Math.floor(timer % 60)
-  const timerMin = Math.floor((timer / 60) % 60)
-  const timerHrs = Math.floor((timer / 60 / 60) % 24)
-  const timerDays = Math.floor(timer / 60 / 60 / 24)
-
-  seconde.textContent = timerSec
-  minute.textContent = timerMin
-  hours.textContent = timerHrs
-  day.textContent = timerDays
+    day.textContent = timerDays
+  }
 }
 
 setInterval(cooldown, 1000)
